eda70eac586243b35b82308877da6f514f387fe4
arifwp <arifwahyu696@gmail.com>
2024-06-07 20:15:20 +0700

    add modular mode switcher

--- src/ColorModeSwitcher.tsx
+++ src/ColorModeSwitcher.tsx
@@ -1,18 +1,22 @@
-import * as React from "react"
 import {
-  useColorMode,
-  useColorModeValue,
+  Icon,
   IconButton,
   IconButtonProps,
-} from "@chakra-ui/react"
-import { FaMoon, FaSun } from "react-icons/fa"
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { RiMoonLine, RiSunLine } from "@remixicon/react";
+import * as React from "react";
 
-type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">
+type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">;
 
 export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
-  const { toggleColorMode } = useColorMode()
-  const text = useColorModeValue("dark", "light")
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun)
+  const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue("dark", "light");
+  const SwitchIcon = useColorModeValue(
+    <Icon as={RiMoonLine} />,
+    <Icon as={RiSunLine} />
+  );
 
   return (
     <IconButton
@@ -22,9 +26,9 @@ export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
       color="current"
       marginLeft="2"
       onClick={toggleColorMode}
-      icon={<SwitchIcon />}
+      icon={SwitchIcon}
       aria-label={`Switch to ${text} mode`}
       {...props}
     />
-  )
-}
+  );
+};

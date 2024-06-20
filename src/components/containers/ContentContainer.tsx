c1073496119ecd8f2c785089ad2c098302dd567f
arifwp <arifwahyu696@gmail.com>
2024-06-20 16:50:26 +0700

    customize

--- src/components/containers/ContentContainer.tsx
+++ src/components/containers/ContentContainer.tsx
@@ -1,25 +1,8 @@
-import {
-  Avatar,
-  Button,
-  HStack,
-  Icon,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuGroup,
-  MenuItem,
-  MenuList,
-  StackProps,
-  Text,
-  VStack,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { RiArrowDownSFill, RiFilter3Fill } from "@remixicon/react";
-import { ColorModeSwitcher } from "../../ColorModeSwitcher";
+import { StackProps, VStack } from "@chakra-ui/react";
 import useScreenWidth from "../../lib/useScreenWidth";
-import { AdminContainer } from "./AdminContainer";
 import { MenuHeader } from "../menu/MenuHeader";
 import { MenuHeaderShrink } from "../menu/MenuHeaderShrink";
+import { AdminContainer } from "./AdminContainer";
 
 interface Props extends StackProps {
   children?: any;
@@ -27,7 +10,6 @@ interface Props extends StackProps {
 }
 
 export const ContentContainer = ({ children, label }: Props) => {
-  const bgComponent = useColorModeValue("gray.100", "gray.800");
   const sw = useScreenWidth();
 
   return (
@@ -41,63 +23,11 @@ export const ContentContainer = ({ children, label }: Props) => {
         spacing={0}
         justify={"start"}
       >
-        {sw >= 500 ? (
+        {sw >= 600 ? (
           <MenuHeader label={label} />
         ) : (
           <MenuHeaderShrink label={label} />
         )}
-
-        {/* <HStack
-          className="header"
-          p={3}
-          align={"center"}
-          w={"100%"}
-          h={"72px"}
-          justify={"space-between"}
-        >
-          <VStack>
-            <Text as={"b"} fontSize={"18px"}>
-              {label}
-            </Text>
-          </VStack>
-
-          <HStack>
-            {label === "Dashboard" && (
-              <Button
-                size={"xs"}
-                py={5}
-                px={7}
-                bg={bgComponent}
-                rightIcon={<Icon as={RiFilter3Fill} />}
-              >
-                Filter
-              </Button>
-            )}
-
-            <ColorModeSwitcher bg={bgComponent} />
-
-            <Menu>
-              <MenuButton
-                as={Button}
-                bgColor={bgComponent}
-                p={4}
-                rightIcon={<Icon as={RiArrowDownSFill} />}
-              >
-                <Avatar size="xs" name="Arif Wahyu" src="" />
-              </MenuButton>
-              <MenuList>
-                <MenuGroup>
-                  <MenuItem>My Account</MenuItem>
-                  <MenuItem>Payments </MenuItem>
-                </MenuGroup>
-                <MenuDivider />
-                <MenuGroup>
-                  <MenuItem>Sign Out</MenuItem>
-                </MenuGroup>
-              </MenuList>
-            </Menu>
-          </HStack>
-        </HStack> */}
 
         {children}
       </VStack>

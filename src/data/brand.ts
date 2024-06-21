a016e5c05d9f1c5e37525ffba6850b7999f290d0
arifwp <arifwahyu696@gmail.com>
2024-06-21 11:38:28 +0700

    add dummy data

---
+++ src/data/brand.ts
@@ -0,0 +1,10 @@
+export interface BrandProps {
+    id: string;
+    name: string;
+}
+
+export const brand: Array<BrandProps> = [
+    { id: '1', name: 'Oppo' },
+    { id: '2', name: 'Asus' },
+    { id: '3', name: 'Realme'},
+];
---
+++ src/data/category.ts
@@ -0,0 +1,13 @@
+export interface CategoryProps {
+    id: string;
+    name: string;
+} 
+
+export const category: Array<CategoryProps> = [
+    { id: '1', name: 'Buah' },
+    { id: '2', name: 'Sayuran' },
+    { id: '3', name: 'Smartphone' },
+    { id: '4', name: 'Laptop' },
+    { id: '5', name: 'Makanan' },
+    { id: '6', name:'Minuman'},
+];

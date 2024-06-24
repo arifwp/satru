d13a29597505ad6538e453d3d62979b94e151831
arifwp <arifwahyu696@gmail.com>
2024-06-24 17:14:12 +0700

    add styling

---
+++ src/components/input/SearchInput.tsx
@@ -0,0 +1,44 @@
+import {
+  Icon,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputProps,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { RiCloseCircleLine, RiSearch2Line } from "@remixicon/react";
+import { useState } from "react";
+
+interface Props extends InputProps {
+  placeholder: string;
+}
+
+export const SearchInput = ({ placeholder, ...rest }: Props) => {
+  const [data, setData] = useState<string>("");
+  const handleChange = (event: any) => setData(event.target.value);
+  const reset = () => setData("");
+
+  return (
+    <InputGroup minW={"150px"} size={"sm"} borderRadius={"xs"} {...rest}>
+      <InputLeftElement pointerEvents="none">
+        <Icon as={RiSearch2Line} />
+      </InputLeftElement>
+      <Input
+        type="text"
+        value={data}
+        onChange={handleChange}
+        placeholder={placeholder}
+      />
+      <InputRightElement>
+        <IconButton
+          size="sm"
+          variant="ghost"
+          icon={<Icon as={RiCloseCircleLine} />}
+          aria-label="Reset input"
+          onClick={reset}
+        />
+      </InputRightElement>
+    </InputGroup>
+  );
+};

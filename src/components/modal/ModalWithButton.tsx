d183d42d572a082e5e8d58488d52147dbebae786
arifwp <arifwahyu696@gmail.com>
2024-06-24 17:13:48 +0700

    optimize

--- src/components/modal/ModalWithButton.tsx
+++ src/components/modal/ModalWithButton.tsx
@@ -1,61 +1,81 @@
 import {
-  Button,
-  Icon,
+  ButtonProps,
   Modal,
+  ModalBody,
   ModalCloseButton,
   ModalContent,
+  ModalFooter,
   ModalHeader,
   ModalOverlay,
-  ModalProps,
+  useColorModeValue,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { RemixiconComponentType } from "@remixicon/react";
+import { useState } from "react";
+import { CButton } from "../CButton";
 
-interface Props extends ModalProps {
+interface BtnProps extends ButtonProps {
   children: any;
   modalTitle: string;
-  buttonTitle: string;
-  isOpen: boolean;
-  bgColorMode?: any;
-  size: string;
-  icon?: RemixiconComponentType;
-  onOpen: () => void;
-  onClose: () => void;
+  modalSize: string;
+  btnIcon?: RemixiconComponentType;
+  btnTitle: string;
+  btnVariant?: any;
+  btnSize?: string;
+  btnBg?: any;
+  btnOnClick?: () => void;
 }
 
-export const ModalWithButton = (
-  {
-    children,
-    modalTitle,
-    buttonTitle,
-    isOpen,
-    icon,
-    bgColorMode,
-    size,
-    onOpen,
-    onClose,
-  }: Props,
-  { ...rest }
-) => {
+export const ModalWithButton = ({
+  children,
+  modalTitle,
+  modalSize,
+  btnTitle,
+  btnIcon,
+  btnBg,
+  btnSize,
+  btnVariant,
+  btnOnClick,
+  ...rest
+}: BtnProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const bgComponent = useColorModeValue("white", "black");
+
   return (
     <>
-      <Button
-        size={"xs"}
-        py={5}
-        px={7}
-        bg={bgColorMode !== undefined ? bgColorMode : undefined}
-        leftIcon={icon !== undefined ? <Icon as={icon} /> : undefined}
+      <CButton
+        size={btnSize}
+        variant={btnVariant}
+        btnColor={btnBg !== undefined ? btnBg : undefined}
+        icon={btnIcon !== undefined ? btnIcon : undefined}
         onClick={onOpen}
         {...rest}
       >
-        {buttonTitle}
-      </Button>
+        {btnTitle}
+      </CButton>
 
-      <Modal isCentered size={size} isOpen={isOpen} onClose={onClose}>
+      <Modal isCentered size={modalSize} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay bg="none" backdropFilter="auto" backdropBlur="5px" />
-        <ModalContent>
+        <ModalContent bg={bgComponent}>
           <ModalHeader>{modalTitle}</ModalHeader>
           <ModalCloseButton />
-          {children}
+          <ModalBody>{children}</ModalBody>
+          <ModalFooter>
+            <CButton variant="solid" onClick={onClose}>
+              Close
+            </CButton>
+
+            <CButton
+              variant="solid"
+              isLoading={loading}
+              loadingText="Loading"
+              spinnerPlacement="start"
+              onClick={btnOnClick}
+            >
+              Close
+            </CButton>
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </>

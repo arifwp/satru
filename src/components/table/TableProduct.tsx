057ff9f0ceac915f3e5c80fb19f8ccf4a23b4415
arifwp <arifwahyu696@gmail.com>
2024-06-24 17:14:44 +0700

    add table component

--- src/components/table/TableProduct.tsx
+++ src/components/table/TableProduct.tsx
@@ -1,5 +1,6 @@
 import {
   AspectRatio,
+  Box,
   Image,
   Table,
   TableCaption,
@@ -13,35 +14,54 @@ import {
 import { product } from "../../data/product";
 
 export const TableProduct = ({ ...rest }) => {
+  const columnHeader = [
+    { key: "foto", label: "Foto" },
+    { key: "name", label: "Nama Produk" },
+    { key: "category", label: "Kategori" },
+    { key: "brand", label: "Merk" },
+    { key: "stock", label: "Stock" },
+  ];
+
   return (
-    <TableContainer className="table-container" w={"100%"} {...rest}>
-      <Table variant={"unstyled"} borderWidth={"1px"}>
-        <TableCaption>Tabel Produk</TableCaption>
-        <Thead borderWidth={"1px"}>
-          <Tr>
-            <Th borderWidth={"1px"}>Foto</Th>
-            <Th borderWidth={"1px"}>Nama Produk</Th>
-            <Th borderWidth={"1px"}>Kategori</Th>
-            <Th borderWidth={"1px"}>Merk</Th>
-            <Th borderWidth={"1px"}>Stok</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {product.map((item, i) => (
-            <Tr borderWidth={"1px"}>
-              <Td borderWidth={"1px"}>
-                <AspectRatio maxW={"180px"} ratio={4 / 3}>
-                  <Image src={item.img} objectFit={"cover"} />
-                </AspectRatio>
-              </Td>
-              <Td>{item.name}</Td>
-              <Td>{item.category.name}</Td>
-              <Td>{item.brand.name}</Td>
-              <Td>{item.stock}</Td>
+    <TableContainer
+      className="table-container"
+      w={"100%"}
+      fontSize={{
+        base: "10px",
+        sm: "12px",
+        md: "12px",
+        lg: "12px",
+        xl: "14px",
+      }}
+      {...rest}
+    >
+      <Box borderWidth={"1px"} borderRadius={"md"} overflow={"hidden"}>
+        <Table variant={"primary"}>
+          <TableCaption>Tabel Produk</TableCaption>
+          <Thead borderWidth={"1px"}>
+            <Tr>
+              {columnHeader.map((item, i) => (
+                <Th key={i}>{item.label}</Th>
+              ))}
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            {product.map((item, i) => (
+              <Tr key={i}>
+                <Td>
+                  <AspectRatio maxW={"100px"} ratio={4 / 3}>
+                    <Image src={item.img} objectFit={"cover"} />
+                  </AspectRatio>
+                </Td>
+                <Td>{item.name}</Td>
+                <Td>{item.category.name}</Td>
+                <Td>{item.brand.name}</Td>
+                <Td>{item.stock}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
     </TableContainer>
   );
 };

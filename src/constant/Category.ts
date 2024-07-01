8ae5da2aab363ec007c49e56ef8aa3496997adde
arifwp <arifwahyu696@gmail.com>
2024-07-01 20:10:53 +0700

    add category const

--- src/constant/Category.ts
+++ src/constant/Category.ts
@@ -1,4 +1,13 @@
 export interface CategoryInterface {
-    id: string;
+    id: any;
     name: string;
 } 
+
+
+export const category: Array<CategoryInterface> = [
+    { id: 1, name: "Laptop" },
+    { id: 2, name: "Komputer" },
+    { id: 3, name: "Smartphone" },
+    { id: 4, name: "Laptop" },
+    { id: 5, name: "Tablet" },
+  ];

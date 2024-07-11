e387dba632422b0aa6ea9b7041efc50308841be7
arifwp <arifwahyu696@gmail.com>
2024-07-11 17:26:35 +0700

    helper to get data user

--- src/utils/helperFunction.ts
+++ src/utils/helperFunction.ts
@@ -1,16 +1,21 @@
 export const formatCurrency = (value: number): string => {
-  return new Intl.NumberFormat('id-ID').format(value);
+  return new Intl.NumberFormat("id-ID").format(value);
 };
 
-export   const formatIDR = (value: string) => {
+export const formatIDR = (value: string) => {
   // Remove non-digit characters
-  const numericValue = value.replace(/\D/g, '');
+  const numericValue = value.replace(/\D/g, "");
 
   // Format the numeric value to currency IDR
-  const formatted = new Intl.NumberFormat('id-ID', {
-    style: 'currency',
-    currency: 'IDR',
+  const formatted = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
   }).format(parseInt(numericValue));
 
   return formatted;
+};
+
+export const getDataUser = () => {
+  var dataUser = localStorage.getItem("user") || "";
+  return JSON.parse(dataUser);
 };

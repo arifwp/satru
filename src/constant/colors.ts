56aba6c5791ed24a6f0548e43571958449bdd597
arifwp <arifwahyu696@gmail.com>
2024-07-12 14:58:42 +0700

    add more color

--- src/constant/colors.ts
+++ src/constant/colors.ts
@@ -1,17 +1,21 @@
-import { useColorModeValue } from "@chakra-ui/react"
+import { useColorModeValue } from "@chakra-ui/react";
 
 export const useBgComponentBaseColor = () => {
-    return useColorModeValue("#F8F9FA", "#1C1C1E");
-}
+  return useColorModeValue("#F8F9FA", "#1C1C1E");
+};
 
 export const useBgHover = () => {
-    return useColorModeValue("#ebedf0", "#ebedf020");;
-}
+  return useColorModeValue("#ebedf0", "#ebedf020");
+};
 
 export const useErrorColor = () => {
-       return useColorModeValue("#E53E3E", "#FC8181");
-}
+  return useColorModeValue("#E53E3E", "#FC8181");
+};
 
 export const useBorderColorInput = () => {
-    return useColorModeValue("RGBA(0, 0, 0, 0.08)", "RGBA(255, 255, 255, 0.16)");;
-}
+  return useColorModeValue("RGBA(0, 0, 0, 0.08)", "RGBA(255, 255, 255, 0.16)");
+};
+
+export const useGreyColor = () => {
+  return useColorModeValue("RGBA(0, 0, 0, 0.36)", "RGBA(255, 255, 255, 0.36)");
+};

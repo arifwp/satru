47eb14ec5c7f623e51f526d7871c454f5a0b5f90
arifwp <arifwahyu696@gmail.com>
2024-07-19 16:43:34 +0700

    reduce opacity on text secondary

--- src/customTheme.ts
+++ src/customTheme.ts
@@ -1,73 +1,74 @@
 import { tableAnatomy } from "@chakra-ui/anatomy";
-import { createMultiStyleConfigHelpers, extendTheme } from '@chakra-ui/react';
+import { createMultiStyleConfigHelpers, extendTheme } from "@chakra-ui/react";
 
-const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(tableAnatomy.keys);
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(tableAnatomy.keys);
 
 const customTheme = extendTheme({
   config: {
-    initialColorMode: 'light',
-    useSystemColorMode:  false,
+    initialColorMode: "light",
+    useSystemColorMode: false,
   },
   colors: {
     w: "white",
     wt: "#eeeeee",
     // dark: '#191919'
-    dark: '#000000'
+    dark: "#000000",
   },
   styles: {
     global: (props: any) => ({
       body: {
-        bg: props.colorMode === 'dark' ? 'dark' : 'white',
-        color: props.colorMode === 'dark' ? 'wt' : 'black',
-      }
+        bg: props.colorMode === "dark" ? "dark" : "white",
+        color: props.colorMode === "dark" ? "wt" : "black",
+      },
     }),
   },
   components: {
     Text: {
       variants: {
         secondary: {
-          opacity: 0.8
-        }
-      }
+          opacity: 0.6,
+        },
+      },
     },
     Modal: {
       baseStyle: (props: any) => ({
         dialog: {
-          bg:props.colorMode === 'dark'?'dark':'white'
-        }
-      })
+          bg: props.colorMode === "dark" ? "dark" : "white",
+        },
+      }),
     },
     Menu: {
       baseStyle: (props: any) => ({
         list: {
-          bg: props.colorMode === 'dark' ? 'dark' : 'white'
+          bg: props.colorMode === "dark" ? "dark" : "white",
         },
         item: {
-          bg: props.colorMode === 'dark' ? 'dark' : 'white'
-        }
-      })
+          bg: props.colorMode === "dark" ? "dark" : "white",
+        },
+      }),
     },
     Link: {
       variants: {
         linkPrimary: {
-          color: 'teal.400',
-          fontWeight: 'semibold',
-          textDecoration: 'none',
-          _hover: {textDecoration: 'none'}
+          color: "teal.400",
+          fontWeight: "semibold",
+          textDecoration: "none",
+          _hover: { textDecoration: "none" },
         },
-      }
+      },
     },
     Button: {
       baseStyle: (props: any) => ({
-        fontWeight: 'semibold',
-        borderRadius: '8px',
+        fontWeight: "semibold",
+        borderRadius: "8px",
       }),
       variants: {
         primaryButton: {
-          bg: 'teal.400',
-          fontWeight: 'semibold'
+          bg: "teal.400",
+          fontWeight: "semibold",
         },
-      }
+      },
     },
     Table: {
       variants: {
@@ -76,31 +77,31 @@ const customTheme = extendTheme({
           return {
             tbody: {
               tr: {
-                '&:nth-of-type(odd)': {
-                  bg: colorMode === 'dark' ? 'dark' : 'white',
+                "&:nth-of-type(odd)": {
+                  bg: colorMode === "dark" ? "dark" : "white",
                 },
-                '&:nth-of-type(even)': {
-                  bg: colorMode === 'dark' ? '#1C1C1E' : '#F8F9FA',
+                "&:nth-of-type(even)": {
+                  bg: colorMode === "dark" ? "#1C1C1E" : "#F8F9FA",
                 },
               },
             },
             tfoot: {
               tr: {
-                '&:last-of-type': {
+                "&:last-of-type": {
                   th: { borderBottomWidth: 0 },
                 },
               },
             },
           };
-        })
-      }
+        }),
+      },
     },
     Form: {
-      parts: ['container', 'requiredIndicator', 'helperText'],
+      parts: ["container", "requiredIndicator", "helperText"],
       baseStyle: {
         container: {
           label: {
-            fontSize: '12px',
+            fontSize: "12px",
           },
         },
       },
@@ -108,7 +109,7 @@ const customTheme = extendTheme({
     FormError: {
       baseStyle: {
         text: {
-          fontSize: '14px',
+          fontSize: "14px",
         },
       },
     },

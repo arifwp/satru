bdb4ed21ce3604e145925f6e7b3966d59d844e48
arifwp <arifwahyu696@gmail.com>
2024-07-24 16:48:46 +0700

    add user interface

---
+++ src/constant/User.ts
@@ -0,0 +1,12 @@
+export interface UserInterface {
+  _id: any;
+  name: string;
+  email: string;
+  phone: string;
+  owner: boolean;
+  bornDate: Date;
+  status: number;
+  createdAt: Date;
+  lastLogin: Date;
+  token: string;
+}

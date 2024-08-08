5eca36a7616d7de955dc6dc02a28f65bcfacc9cc
arifwp <arifwahyu696@gmail.com>
2024-08-08 16:57:22 +0700

    remove image

--- src/components/card/ItemCart.tsx
+++ src/components/card/ItemCart.tsx
@@ -5,73 +5,59 @@ export const ItemCart = () => {
   const borderColor = useBorderColorInput();
 
   return (
-    <HStack
+    <VStack
       w={"100%"}
       p={2}
-      borderWidth={"1px"}
-      borderColor={borderColor}
-      borderRadius={"md"}
+      borderBottomWidth={"1px"}
+      borderBottomColor={borderColor}
     >
-      <Image
-        src="https://placehold.co/600x400"
-        objectFit={"cover"}
-        w={"100%"}
-        maxW={"72px"}
-        borderRadius={"md"}
-      />
-      <VStack w={"100%"} align={"stretch"}>
-        <Text
-          fontSize={[12, null, 14]}
-          noOfLines={1}
-          textOverflow={"ellipsis"}
-          fontWeight={600}
-        >
-          {/* Lorem Ipsum is */}
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries
-        </Text>
+      <Text
+        fontSize={[12, null, 14]}
+        noOfLines={1}
+        textOverflow={"ellipsis"}
+        fontWeight={600}
+      >
+        {/* Lorem Ipsum is */}
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries
+      </Text>
 
-        <HStack justify={"space-between"}>
-          <HStack borderWidth={"1px"} borderColor={borderColor}>
-            <HStack
-              py={0.5}
-              px={2}
-              borderRightWidth={"1px"}
-              borderColor={borderColor}
-            >
-              <Text fontSize={[12, null, 14]} variant={"secondary"}>
-                -
-              </Text>
-            </HStack>
-            <HStack>
-              <Text fontSize={[12, null, 14]} variant={"secondary"}>
-                12
-              </Text>
-            </HStack>
-            <HStack
-              py={0.5}
-              px={2}
-              borderLeftWidth={"1px"}
-              borderColor={borderColor}
-            >
-              <Text fontSize={[12, null, 14]} variant={"secondary"}>
-                +
-              </Text>
-            </HStack>
-          </HStack>
-
-          <Text
-            fontSize={[12, null, 14]}
-            fontWeight={600}
-            variant={"secondary"}
+      {/* <HStack justify={"space-between"}>
+        <HStack borderWidth={"1px"} borderColor={borderColor}>
+          <HStack
+            py={0.5}
+            px={2}
+            borderRightWidth={"1px"}
+            borderColor={borderColor}
           >
-            24.000
-          </Text>
+            <Text fontSize={[12, null, 14]} variant={"secondary"}>
+              -
+            </Text>
+          </HStack>
+          <HStack>
+            <Text fontSize={[12, null, 14]} variant={"secondary"}>
+              12
+            </Text>
+          </HStack>
+          <HStack
+            py={0.5}
+            px={2}
+            borderLeftWidth={"1px"}
+            borderColor={borderColor}
+          >
+            <Text fontSize={[12, null, 14]} variant={"secondary"}>
+              +
+            </Text>
+          </HStack>
         </HStack>
-      </VStack>
-    </HStack>
+
+        <Text fontSize={[12, null, 14]} fontWeight={600} variant={"secondary"}>
+          24.000
+        </Text>
+      </HStack> */}
+    </VStack>
   );
 };

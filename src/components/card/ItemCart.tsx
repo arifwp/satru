3fa83bccb30d5f8e11eafa692e65422496f7ee06
arifwp <arifwahyu696@gmail.com>
2024-08-12 16:56:58 +0700

    cbange increment and decremnt function

--- src/components/card/ItemCart.tsx
+++ src/components/card/ItemCart.tsx
@@ -1,21 +1,40 @@
-import { HStack, Image, StackProps, Text, VStack } from "@chakra-ui/react";
-import { useBorderColorInput } from "../../constant/colors";
-import { TransactionInterface } from "../../constant/Transaction";
+import { Button, HStack, StackProps, Text, VStack } from "@chakra-ui/react";
+import {
+  useBorderColorInput,
+  useTextPrimaryColor,
+} from "../../constant/colors";
+import { ProductCartInterface } from "../../constant/Transaction";
+import { useTransactionStore } from "../../store/useTransactionStore";
 
 interface Props extends StackProps {
-  data: TransactionInterface;
+  data: ProductCartInterface[];
 }
 
 export const ItemCart = ({ data, ...rest }: Props) => {
   const borderColor = useBorderColorInput();
+  const txtColor = useTextPrimaryColor();
+  const { updateProduct, removeProduct } = useTransactionStore();
+
+  const increment = (item: ProductCartInterface) => {
+    if (item.qty < item.stock) {
+      updateProduct(item._id, { qty: (item?.qty ?? 0) + 1 });
+    }
+  };
+
+  const decrement = (item: ProductCartInterface) => {
+    if (item.qty > 1) {
+      updateProduct(item._id, { qty: (item?.qty ?? 0) - 1 });
+    }
+  };
 
   return (
     <VStack w={"100%"}>
-      {data.product.map((item, i) => (
+      {data.map((item, i) => (
         <VStack
           key={item._id}
           w={"100%"}
-          p={2}
+          pb={2}
+          align={"stretch"}
           borderBottomWidth={"1px"}
           borderBottomColor={borderColor}
         >
@@ -28,6 +47,94 @@ export const ItemCart = ({ data, ...rest }: Props) => {
           >
             {item.name}
           </Text>
+
+          <HStack justify={"space-between"} px={2} fontSize={[10, null, 12]}>
+            <Text variant={"secondary"}>Qty</Text>
+            <HStack
+              borderWidth={"1px"}
+              borderColor={borderColor}
+              w={"fit-content"}
+            >
+              <HStack
+                borderRightWidth={"1px"}
+                borderColor={borderColor}
+                cursor={"pointer"}
+                onClick={() => decrement(item)}
+              >
+                <Button
+                  py={0.5}
+                  px={2}
+                  borderRadius={"none"}
+                  size={"xs"}
+                  variant={"ghost"}
+                  colorScheme="teal"
+                  fontSize={[10, null, 12]}
+                  fontWeight={"bold"}
+                >
+                  -
+                </Button>
+              </HStack>
+              <HStack p={2}>
+                <Text fontSize={[10, null, 12]}>{item.qty}</Text>
+              </HStack>
+              <HStack
+                borderLeftWidth={"1px"}
+                borderColor={borderColor}
+                cursor={"pointer"}
+                onClick={() => increment(item)}
+              >
+                <Button
+                  py={0.5}
+                  px={2}
+                  borderRadius={"none"}
+                  size={"xs"}
+                  variant={"ghost"}
+                  colorScheme="teal"
+                  fontSize={[10, null, 12]}
+                  fontWeight={"bold"}
+                >
+                  +
+                </Button>
+              </HStack>
+            </HStack>
+          </HStack>
+
+          {(item.variants as any)?.length > 0 && (
+            <HStack
+              justify={"space-between"}
+              mt={2}
+              px={2}
+              fontSize={[10, null, 12]}
+            >
+              <Text variant={"secondary"}>Varian</Text>
+
+              {item.variants?.map((variant) => (
+                <Text
+                  key={variant._id}
+                  w={"100%"}
+                  align={"end"}
+                  fontSize={[12, null, 14]}
+                  noOfLines={1}
+                  textOverflow={"ellipsis"}
+                >
+                  {variant.variantName}
+                </Text>
+              ))}
+            </HStack>
+          )}
+
+          <Button
+            w={"fit-content"}
+            size={"xs"}
+            variant={"ghost"}
+            color={txtColor}
+            colorScheme="teal"
+            fontSize={[10, null, 12]}
+            fontWeight={"normal"}
+            onClick={() => removeProduct(item._id)}
+          >
+            Hapus
+          </Button>
         </VStack>
       ))}
     </VStack>

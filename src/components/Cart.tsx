412f06fd2d45995840b2334ca30e081a1c23ea3c
arifwp <arifwahyu696@gmail.com>
2024-08-12 16:55:14 +0700

    change passing data and remove unused import

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,20 +1,13 @@
 import { VStack } from "@chakra-ui/react";
-import { ItemCart } from "./card/ItemCart";
-import { useEffect } from "react";
 import { useTransactionStore } from "../store/useTransactionStore";
+import { ItemCart } from "./card/ItemCart";
 
 export const Cart = ({ ...rest }) => {
-  const { transactions, addTransaction, updateTransaction, clearTransaction } =
-    useTransactionStore();
+  const { products } = useTransactionStore();
 
   return (
     <VStack w={"100%"} {...rest}>
-      {transactions && <ItemCart data={transactions} />}
-      {/* <ItemCart />
-      <ItemCart />
-      <ItemCart />
-      <ItemCart />
-      <ItemCart /> */}
+      {products && <ItemCart data={products} />}
     </VStack>
   );
 };

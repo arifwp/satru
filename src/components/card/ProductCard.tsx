175121cf0df800394dd8ec79998b9d6af7343a58
arifwp <arifwahyu696@gmail.com>
2024-08-16 17:02:44 +0700

    add search feature and pagination

--- src/components/card/ProductCard.tsx
+++ src/components/card/ProductCard.tsx
@@ -1,5 +1,8 @@
 import {
+  Button,
+  HStack,
   Image,
+  Select,
   StackProps,
   Text,
   useDisclosure,
@@ -19,12 +22,21 @@ import formatNumber from "../../lib/formatNumber";
 import { useTransactionStore } from "../../store/useTransactionStore";
 import { getDataUser } from "../../utils/helperFunction";
 import { TransactionDrawer } from "../drawer/dedicated/TransactionDrawer";
+import { Empty } from "../Empty";
+import { TableSkeleton } from "../TableSkeleton";
 
 interface Props extends StackProps {
   filterOutlet: SelectOption[] | undefined;
   filterCategory: SelectOption[] | undefined;
   filterSearch: string;
 }
+
+const rowOptions = [
+  { id: 1, name: 20 },
+  { id: 2, name: 30 },
+  { id: 3, name: 60 },
+  { id: 4, name: 100 },
+];
 
 export const ProductCard = ({
   filterOutlet,
@@ -41,13 +53,22 @@ export const ProductCard = ({
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { products, addTransaction, addProduct, updateProduct } =
     useTransactionStore();
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [totalItems, setTotalItems] = useState<number | undefined>(undefined);
+  const [limitPagination, setLimitPagination] = useState<number>(20);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const pagesArr = Array.from({ length: totalPages as any }, (_, i) => i + 1);
 
   useEffect(() => {
     const token = getCookie("token");
     const ownerId = getDataUser().ownerId
       ? getDataUser().ownerId
       : getDataUser()._id;
+
     let url;
+    let outletIds;
+    let categoryIds;
 
     if (
       filterOutlet &&
@@ -56,36 +77,51 @@ export const ProductCard = ({
       filterCategory.length !== 0
     ) {
       const otltIds = filterOutlet.map((item) => item._id);
-      const outletIds = otltIds.join(",");
+      outletIds = otltIds.join(",");
 
       const ctgIds = filterCategory.map((item) => item._id);
-      const categoryIds = ctgIds.join(",");
+      categoryIds = ctgIds.join(",");
 
-      url = `${process.env.REACT_APP_API_URL}/v1/product/getAllProductByOutletCategory/${ownerId}/${outletIds}/${categoryIds}`;
+      url = `${process.env.REACT_APP_API_URL}/v1/product/getAllProductByOutletCategory`;
     } else if (filterCategory && filterCategory.length > 0) {
       const ctgIds = filterCategory.map((item) => item._id);
-      const categoryIds = ctgIds.join(",");
+      categoryIds = ctgIds.join(",");
 
-      url = `${process.env.REACT_APP_API_URL}/v1/product/getAllProductByCategory/${ownerId}/${categoryIds}`;
+      url = `${process.env.REACT_APP_API_URL}/v1/product/getAllProductByCategory`;
     } else if (filterOutlet && filterOutlet.length > 0) {
       const otltIds =
         filterOutlet && (filterOutlet as any[]).map((item) => item._id);
-      const outletIds = otltIds && (otltIds as string[]).join(",");
+      outletIds = otltIds && (otltIds as string[]).join(",");
 
-      url = `${process.env.REACT_APP_API_URL}/v1/product/getAllProductByOutlet/${ownerId}/${outletIds}`;
+      url = `${process.env.REACT_APP_API_URL}/v1/product/getAllProductByOutlet`;
     } else {
-      url = `${process.env.REACT_APP_API_URL}/v1/product/getAllProduct/${ownerId}`;
+      url = `${process.env.REACT_APP_API_URL}/v1/product/getAllProduct`;
     }
+
+    const request = {
+      ownerId: ownerId,
+      page: currentPage,
+      limit: limitPagination,
+      outletIds: outletIds,
+      categoryIds: categoryIds,
+      search: filterSearch,
+    };
 
     url &&
       axios
-        .get(url, {
+        .post(url, request, {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
         })
         .then((response: AxiosResponse) => {
+          setTotalItems(
+            JSON.parse(response.request.response).pagination.totalItems
+          );
+          setTotalPages(
+            JSON.parse(response.request.response).pagination.totalPages
+          );
           setData(JSON.parse(response.request.response).data);
         })
         .catch((error: AxiosError) => {
@@ -98,7 +134,14 @@ export const ProductCard = ({
         .finally(() => {
           setLoaded(true);
         });
-  }, [filterOutlet, filterSearch, filterCategory, toast]);
+  }, [
+    filterOutlet,
+    filterSearch,
+    filterCategory,
+    toast,
+    currentPage,
+    limitPagination,
+  ]);
 
   useEffect(() => {
     let total = 0;
@@ -155,13 +198,27 @@ export const ProductCard = ({
     }
   };
 
-  useEffect(() => {
-    console.log("zustand produk", products);
-  }, [products]);
+  const handleSelect = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setLimitPagination(parseInt(value));
+  };
+
+  if (!loaded) {
+    return <TableSkeleton row={5} column={10} px={4} />;
+  } else if (loaded && data && data.length < 1) {
+    return <Empty title="Produk tidak ditemukan" mt={6} />;
+  }
 
   return (
     <>
-      <VStack w={"100%"} mt={2} overflowY={"auto"} {...rest}>
+      <VStack
+        w={"100%"}
+        h={"100vh"}
+        mt={2}
+        overflowY={"auto"}
+        position={"relative"}
+        {...rest}
+      >
         <Wrap
           className="scrollY"
           overflowY={"auto"}
@@ -208,6 +265,58 @@ export const ProductCard = ({
             </WrapItem>
           ))}
         </Wrap>
+
+        <HStack
+          w={"100%"}
+          bg={"transparent"}
+          backdropFilter="blur(5px)"
+          px={4}
+          pb={4}
+          position={"absolute"}
+          bottom={0}
+          justify={"space-between"}
+        >
+          {totalItems && (
+            <HStack>
+              <Select
+                w={"fit-content"}
+                onChange={handleSelect}
+                isDisabled={
+                  (totalItems as any) > rowOptions[0].name ? false : true
+                }
+              >
+                {rowOptions[0].name < (totalItems as any) ? (
+                  rowOptions.map((item, i) => (
+                    <option key={i} value={item.name}>
+                      {item.name}
+                    </option>
+                  ))
+                ) : (
+                  <option value={totalItems}>{totalItems}</option>
+                )}
+              </Select>
+
+              <Text>{`dari ${totalItems}`}</Text>
+            </HStack>
+          )}
+
+          {totalPages && (
+            <HStack>
+              {pagesArr.map((page: any, i: any) => (
+                <Button
+                  key={i}
+                  size={"sm"}
+                  colorScheme="teal"
+                  borderRadius={"md"}
+                  variant={"outline"}
+                  onClick={() => setCurrentPage(page)}
+                >
+                  {page}
+                </Button>
+              ))}
+            </HStack>
+          )}
+        </HStack>
       </VStack>
 
       <TransactionDrawer

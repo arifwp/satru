68f2ce8c13d2f61eb7fc2da44d87da0e27d63b97
arifwp <arifwahyu696@gmail.com>
2024-08-29 12:11:04 +0700

    add outlet interface to it

--- src/constant/Discount.ts
+++ src/constant/Discount.ts
@@ -1,7 +1,10 @@
+import { OutletInterface } from "./Outlet";
+
 export interface DiscountInterface {
   _id: any;
   ownerId: any;
   outletId: any;
+  outlet: OutletInterface[];
   name: string;
   discountType: number;
   discount: number;

9409c0b5217c2772e82c3fbf864503a3a891a1ac
arifwp <arifwahyu696@gmail.com>
2024-08-29 16:09:10 +0700

    add new interface

--- src/constant/Outlet.ts
+++ src/constant/Outlet.ts
@@ -1,3 +1,9 @@
+interface BusinessTypesInterface {
+  id: number;
+  name: string;
+  optGroup: boolean;
+}
+
 export interface OutletInterface {
   _id: any;
   name: string;
@@ -5,5 +11,17 @@ export interface OutletInterface {
   estEmployee: number;
   address: string;
   typeId: any;
+  typeoutlet: BusinessTypesInterface;
   createdAt: Date;
 }
+
+export const businessType: Array<BusinessTypesInterface> = [
+  { id: 1, name: "Makanan & Minuman", optGroup: true },
+  { id: 2, name: "Kafe / Coffe Shop", optGroup: false },
+  { id: 3, name: "Restoran", optGroup: false },
+  { id: 4, name: "Roti, Kue & Camilan", optGroup: false },
+  { id: 5, name: "Retail", optGroup: true },
+  { id: 6, name: "Toko Kelontong & Retail", optGroup: false },
+  { id: 7, name: "Minimarket", optGroup: false },
+  { id: 8, name: "Vape Store", optGroup: false },
+];

6e099bb95ef256403c0f3d12ce81555321f6c22c
arifwp <arifwahyu696@gmail.com>
2024-08-30 16:50:35 +0700

    add new data constant

--- src/components/table/CTable.tsx
+++ src/components/table/CTable.tsx
@@ -18,6 +18,7 @@ import {
 import { RiArrowDownLine, RiArrowUpLine } from "@remixicon/react";
 import React from "react";
 import { useBgComponentBaseColor } from "../../constant/colors";
+import { rowOptions } from "../../constant/utilsConstant";
 
 interface Props extends TableContainerProps {
   columnHeader: any;
@@ -29,13 +30,6 @@ interface Props extends TableContainerProps {
   onPageChange?: (page: number) => void;
   onLimitChange?: (limit: number) => void;
 }
-
-const rowOptions = [
-  { id: 1, name: 10 },
-  { id: 2, name: 20 },
-  { id: 3, name: 50 },
-  { id: 4, name: 100 },
-];
 
 export const CTable = ({
   columnHeader,
---
+++ src/constant/utilsConstant.ts
@@ -0,0 +1,6 @@
+export const rowOptions = [
+  { id: 1, name: 10 },
+  { id: 2, name: 20 },
+  { id: 3, name: 50 },
+  { id: 4, name: 100 },
+];

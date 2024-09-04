79528b16cc3e5bf694f6309d31ee1656d5df6c8e
arifwp <arifwahyu696@gmail.com>
2024-09-04 17:11:14 +0700

    styling

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -7,7 +7,7 @@ interface Props extends StackProps {
 
 export const Empty = ({ title, subTitle, ...rest }: Props) => {
   return (
-    <VStack className="empty-container" {...rest}>
+    <VStack className="empty-container" textAlign={"center"} {...rest}>
       <Image src="/assets/svg/illustration_empty.svg" />
       <Text fontWeight={"semibold"}>{title}</Text>
       <Text variant={"secondary"}>{subTitle}</Text>

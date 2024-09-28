4df45df3fd9c9a831334bfbd9fd50f86332f6f23
arifwp <arifwahyu696@gmail.com>
2024-09-28 23:47:25 +0700

    add new function

--- src/utils/helperFunction.ts
+++ src/utils/helperFunction.ts
@@ -1,4 +1,12 @@
 import moment from "moment";
+import { useTransactionStore } from "../store/useTransactionStore";
+import {
+  ManualTransactionInterface,
+  ProductCartInterface,
+} from "../constant/Transaction";
+import { DiscountInterface } from "../constant/Discount";
+import formatNumber from "../lib/formatNumber";
+import { SelectOption } from "../constant/SelectOption";
 
 export const formatCurrency = (value: number): string => {
   return new Intl.NumberFormat("id-ID").format(value);
@@ -44,4 +52,186 @@ export const getUserOrAdminId = () => {
   const id = getDataUser().ownerId ? getDataUser().ownerId : getDataUser()._id;
 
   return id;
+};
+
+interface CountTotalPrice {
+  products: ProductCartInterface[];
+  manualTransaction: ManualTransactionInterface[];
+  applyDiscount: DiscountInterface;
+  tax: number;
+}
+
+export const showPrice = (item: ProductCartInterface) => {
+  let total = 0;
+
+  total += item.price * item.qty;
+
+  // cek apakah ada variant
+  if (item && item.variants && item.variants.length > 0) {
+    item.variants.map((variant) => {
+      const variantPrice = variant.variantPrice;
+      total = variantPrice * item.qty;
+
+      if (item.discount && item.discountType.id === 1) {
+        total = variantPrice * item.qty - item.discount || 0;
+      } else if (item.discount && item.discountType.id === 2) {
+        // total = variantPrice - item.discount || 0;
+        let countDiscountVariant = (variantPrice * item.discount) / 100;
+        let checkQtyVariant = countDiscountVariant * item.qty;
+        total = variantPrice * item.qty - checkQtyVariant;
+      }
+    });
+
+    //jika tidak ada variant
+  } else if (item.discount && item.discountType.id === 1) {
+    total = item.price * item.qty - item.discount || 0;
+
+    // return `Rp ${formatNumber(total)}`;
+  } else if (item.discount && item.discountType.id === 2) {
+    // total = item.price * item.qty - item.discount || 0;
+    let countDiscount = (item.price * item.discount) / 100;
+    let checkQty = countDiscount * item.qty;
+    total = item.price * item.qty - checkQty;
+  }
+
+  return `Rp ${formatNumber(total)}`;
+};
+
+export const showSubTotal = (
+  products: ProductCartInterface[],
+  manualTransaction: ManualTransactionInterface[],
+  applyDiscount: SelectOption | undefined
+) => {
+  let total: number = 0;
+  let totalProductPrice: number = 0;
+  let totalManualTransaction: number = 0;
+
+  if (products.length > 0) {
+    products.map((item) => {
+      if (item.finalPrice) {
+        totalProductPrice += item.finalPrice;
+      }
+    });
+  }
+
+  if (manualTransaction.length > 0) {
+    manualTransaction.map((item) => {
+      totalManualTransaction += item.price;
+    });
+  }
+
+  return `Rp ${
+    products.length > 0 || manualTransaction.length > 0
+      ? formatNumber(totalProductPrice + totalManualTransaction)
+      : 0
+  }`;
+};
+
+export const showTotalDiscount = (
+  products: ProductCartInterface[],
+  applyDiscount: SelectOption | undefined
+) => {
+  let totalPercent = 0;
+  let totalRp = 0;
+
+  products.map((item, i) => {
+    if (!!item.discountType) {
+      if (item.discountType.id === 2) {
+        totalPercent += parseInt(item.discount.toString());
+      }
+
+      if (item.discountType.id === 1) {
+        totalRp += parseInt(item.discount.toString());
+      }
+    }
+  });
+
+  if (!!applyDiscount) {
+    if ((applyDiscount as DiscountInterface).discountType === 2) {
+      totalPercent += (applyDiscount as DiscountInterface).discount;
+    } else if ((applyDiscount as DiscountInterface).discountType === 1) {
+      totalRp += (applyDiscount as DiscountInterface).discount;
+    }
+  }
+
+  return `${!!totalRp ? `Rp ${formatNumber(totalRp)}` : ""} ${
+    !!totalRp && !!totalPercent ? `+` : " "
+  } ${!!totalPercent ? `${totalPercent}%` : "-"}`;
+};
+
+export const showTotalPrice = (
+  products: ProductCartInterface[],
+  manualTransaction: ManualTransactionInterface[],
+  applyDiscount: SelectOption | undefined,
+  tax: number | undefined
+) => {
+  let endPrice: number = 0;
+  let priceAfterTax: number = 0;
+  let totalManualTransaction: number = 0;
+
+  if (manualTransaction.length > 0) {
+    manualTransaction.map((item) => {
+      totalManualTransaction += item.price;
+    });
+  }
+
+  // count sub total
+  let subTotal: number = 0;
+
+  if (products.length > 0) {
+    products.map((item) => {
+      if (item.finalPrice) {
+        subTotal += item.finalPrice;
+      }
+    });
+  }
+
+  // count subTotal - tax
+  if (!!tax) {
+    const countTax = (subTotal + totalManualTransaction * tax) / 100;
+    priceAfterTax = subTotal + countTax;
+  }
+
+  endPrice = priceAfterTax + totalManualTransaction;
+
+  //count total discount
+  let totalPercent = 0;
+  let totalRp = 0;
+
+  products.map((item, i) => {
+    if (!!item.discountType) {
+      if (item.discountType.id === 2) {
+        totalPercent += parseInt(item.discount.toString());
+      }
+
+      if (item.discountType.id === 1) {
+        totalRp += parseInt(item.discount.toString());
+      }
+    }
+  });
+
+  if (!!applyDiscount) {
+    if ((applyDiscount as DiscountInterface).discountType === 2) {
+      totalPercent += (applyDiscount as DiscountInterface).discount;
+    } else if ((applyDiscount as DiscountInterface).discountType === 1) {
+      totalRp += (applyDiscount as DiscountInterface).discount;
+    }
+  }
+
+  // count final price
+  if (!!applyDiscount) {
+    if ((applyDiscount as DiscountInterface).discountType === 2) {
+      const countDiscount =
+        (endPrice *
+          parseInt((applyDiscount as DiscountInterface).discount.toString())) /
+        100;
+      // const checkQty = countDiscount * products.length;
+      // endPrice = priceAfterTax * products.length - checkQty;
+      endPrice = endPrice - countDiscount;
+    } else if ((applyDiscount as DiscountInterface).discountType === 1) {
+      endPrice = endPrice - totalRp;
+    }
+  }
+
+  return `Rp ${endPrice !== 0 ? formatNumber(endPrice) : endPrice}`;
 };
